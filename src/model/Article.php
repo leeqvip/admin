1fc1a52506ce689deb833d2ebaf917147fc1cbec
TechLee <techlee@qq.com>
2018-10-30 18:13:56 +0800

    udpate

--- src/model/Article.php
+++ src/model/Article.php
@@ -19,6 +19,31 @@ class Article extends Model
         return $summary;
     }
 
+    //上一篇
+    public function previous()
+    {
+        $prev = $this->where('sort', 'elt', $this->sort)
+            ->field('id,title')
+            ->where('updated_at', 'elt', $this->updated_at)
+            ->where('id', 'lt', $this->id)
+            ->where('category_id', 'eq', $this->category_id)
+            ->order(['sort' => 'asc', 'updated_at' => 'desc', 'id' => 'desc'])
+            ->find();
+        return $prev;
+    }
+
+    public function next()
+    {
+        $next = $this->where('sort', 'egt', $this->sort)
+            ->field('id,title')
+            ->where('updated_at', 'egt', $this->updated_at)
+            ->where('id', 'gt', $this->id)
+            ->where('category_id', 'eq', $this->category_id)
+            ->order(['sort' => 'desc', 'updated_at' => 'asc', 'id' => 'asc'])
+            ->find();
+        return $next;
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- src/model/Tag.php
+++ src/model/Tag.php
@@ -8,4 +8,8 @@ class Tag extends Model
 {
     protected $table = 'techadmin_tags';
 
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, ArticleTag::class, 'article_id', 'tag_id');
+    }
 }
